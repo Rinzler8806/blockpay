a0ef874f62d9f6c850a8e40371988136dbdc577b
Rinzler8806 <shane.rogers916@gmail.com>
2022-02-21 16:30:26 -0800

    Updated Welcome page - Added buttons to connect to wallet and login. Added input form for sending tokens

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const NavbarItem = ({ title, classProps }) => {
 const Navbar = () => {
     const [ toggleMenu, setToggleMenu ] = useState(false);
     return (
-        <nav className='w-full flex md:justify-center'>
+        <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
                 <img src={logo} alt="logo" className='w-32 cursor-pointer'></img>
             </div>
